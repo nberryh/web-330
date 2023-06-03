c4bea7473c25badd61de1c0aa1e50640ec7526b5
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-06-03 18:22:13 -0500

    merge

--- week-2/berryhill-palindrome.js
+++ week-2/berryhill-palindrome.js
@@ -20,10 +20,18 @@ function isPalindrome(str) {
     return reverse(str) === str;
 }
 function showResults() {
-    var txtPhrase = document.getElementById("txtPhrase").value;
+    var originalPhrase = document.getElementById("txtPhrase").value;
     var assignResults = document.getElementById("assign-results");
     var today = getTodaysDate();
-    var len = getLength(txtPhrase);
-    var reversedPhrase = reverse(txtPhrase);
-    var header = `Today's Date: ${today}<br>Phrase Length: ${len}<br>Reversed Phrase: ${reversedPhrase}`;
+    var len = getLength(originalPhrase);
+    var reversedPhrase = reverse(originalPhrase);
+    var header = `Today's Date: ${today}<br> Original Phrase: ${originalPhrase}<br>Reversed Phrase: ${reversedPhrase}<br>Phrase Length: ${len}<br>`;
+
+    if (isPalindrome(originalPhrase.toLowerCase())) {
+            assignResults.innerHTML = `${originalPhrase} <strong><u>is</u></strong> a palindrome`;
+        } else {
+            assignResults.innerHTML = `${originalPhrase} <strong><u>is</u></strong> a palindrome`;
+        }
+    
+        document.getElementById("assign-results-header").innerHTML = header;
 }
