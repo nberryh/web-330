7c2b980e51c5c791a0a9c1ef2ac6ee9bcd118225
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-07-08 17:37:52 -0500

    merge

---
+++ week-7/theme.js
@@ -0,0 +1,27 @@
+/*
+===========================================
+; Title:  theme.js
+; Author: Nolan Berryhill
+; Date:   09 July 2023
+; Description: Applying strings to list in html
+;==========================================
+*/
+
+//Function for setDefaultTheme
+function setDefaultTheme() {
+
+    //Assigns variables
+    const theme = localStorage.getItem("mode") || "light-theme";
+    const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+    const iconText = localStorage.getItem("iconText") || "Light Mode";
+
+    //Assigns output to HTML
+    document.body.classList.value = theme;
+    document.getElementById("icon-mode").classList.add(iconMode);
+    document.getElementById("icon-text").innerHTML = iconText;
+}
+
+//Function for setSelectedTheme
+function setSelectedTheme() {
+    document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+}
